7333ccaaba08d5ffc683994c7432c49ac47c9d7b
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2025-09-24 16:46:45 -0400

    initial commit

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,69 @@
+import java.util.Properties
+
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(localPropertiesFile.reader())
+}
+
+// Lê a chave do Maps do arquivo de propriedades
+val mapsApiKey = localProperties.getProperty("MAPS_API_KEY")
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+
+}
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
+
+
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    //
+}
+
+
+android {
+    namespace = "com.example.o_auth2"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.o_auth2"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+        manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
+
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/com/example/o_auth2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.o_auth2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
