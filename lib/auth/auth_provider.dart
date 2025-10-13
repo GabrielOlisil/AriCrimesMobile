661f56977911bfd82f60991be3f802498369de33
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2025-10-13 16:02:58 -0400

    bkp

--- lib/auth/auth_provider.dart
+++ lib/auth/auth_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
-
+import 'dart:developer';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:http/http.dart' as http;
@@ -15,7 +16,7 @@ class MyAuthProvider extends ChangeNotifier {
 
   final appAuth = FlutterAppAuth();
 
-  static const scopes = ['openid'];
+  static const scopes = ['openid', 'email', 'profile'];
 
   String? _idToken;
 
@@ -27,14 +28,37 @@ class MyAuthProvider extends ChangeNotifier {
 
   MyAuthProvider() {
     print('inicializando auth provider');
-    if (!isAuthenticated) {
+    if (isAuthenticated) {
+      return;
+    }
+
+    if(!kIsWeb){
       signIn();
     }
+
+    //será aqui
   }
 
   void initAuthState() {}
 
-  Future<void> _sighInWeb() async {}
+  Future<void> _handleWebAuthFlow() async {
+    final uri = html.window.location.href;
+    final urlParams = Uri.parse(uri).queryParameters;
+
+    if (urlParams.containsKey('code')) {
+      // 1. O Keycloak REDIRECIONOU de volta com o 'code' de autorização
+      log("Código de autorização encontrado na URL. Trocando por tokens...");
+      await _exchangeCodeForTokens(urlParams['code']!);
+    } else if (urlParams.containsKey('error')) {
+      // 2. Erro do Keycloak (ex: acesso negado)
+      _errorMessage = 'Erro de autorização: ${urlParams['error']}';
+      _resetAuth();
+    } else {
+      // 3. Nenhum código encontrado. Redirecionar para o Keycloak.
+      log("Nenhum código encontrado. Redirecionando para o Keycloak...");
+      await _redirectToKeycloakLogin();
+    }
+  }
 
   Future<void> signIn() async {
     try {
@@ -50,6 +74,10 @@ class MyAuthProvider extends ChangeNotifier {
       );
 
       var tokenRaw = result.idToken;
+
+      log("ACCESS TOKEN: ${result.accessToken}");
+      log("ID TOKEN: ${result.idToken}");
+
 
       if (tokenRaw != null) {
         var token = Jwt.parseJwt(tokenRaw);
